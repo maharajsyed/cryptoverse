5b4ab73a35ec5e11b6c787a45d7433bc05f93bf4
maharajsyed <maharajsyed@gmail.com>
2022-01-24 00:23:00 -0500

    base setup

---
+++ cryptoverse/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons';
+
+import icon from '../images/cryptoverse.png'
+
+const Navbar = () => {
+  return (
+      <div className='nav-container'>
+          <div className='logo-container'>
+              <Avatar src={icon} size="large"/>
+              <Typography.title level={2} className="logo">
+                  <Link to="/">Cryptoverse</Link>
+              </Typography.title>
+              {/*<Button className="menu-control-container"></Button> */}
+          </div>
+      </div>
+  );
+};
+
+
+export default Navbar;
